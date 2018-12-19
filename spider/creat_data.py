2ed384286be5b3776d3d2fbe6a5fe4ce42302fa8
puxiaoshuai <1372553910@qq.com>
2018-12-19 17:10:43 +0800

    fix #15 企业列表界面

--- spider/creat_data.py
+++ spider/creat_data.py
@@ -3,13 +3,9 @@ from jobplus.models import User, Company, Job, db
 from jobplus import create_app
 from faker import Faker
 
-
 app = create_app("development")
 app.app_context().push()
 faker = Faker()
-
-
-
 
 # 生成一个用户boss
 def create_boss(company):
